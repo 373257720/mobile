a1a8deeb89ea31ea87c1a6d9462c52dbe40a4b26
white <373257720@qq.com>
2020-12-12 21:05:54 +0800

    12-12

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   ENV_CONFIG:'"dev"',
-  BASE_API: "'http://192.168.1.45:8080'",
+  BASE_API: "'http://47.90.62.114:8086'",
   WEB_API:'"http://192.168.1.17:8080"',
 })
